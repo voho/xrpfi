1ad1cb552e08758580278dba5e6b864598bbd9d0
Vojta Hordejcuk <vojtech.hordejcuk@gmail.com>
2019-11-09 17:24:32 +0100

    moving legacy files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,34 @@
-import React, {useEffect, useState} from "react";
+import "normalize.css";
+import React, {useEffect, useReducer} from "react";
+import {BrowserRouter, Redirect, Route, Switch} from "react-router-dom";
+import "./App.css";
+import {NewsCenter} from "./component/news/NewsCenter";
+import {scheduleRegularNewsUpdate, scheduleRegularTickersUpdate} from "./service/api";
+import {NewsState, TickersState} from "./service/model";
+import {newsReducer, UseNewsReducerContext} from "./service/NewsReducer";
+import {tickersReducer, UseTickersReducerContext} from "./service/TickersReducer";
 
-const App: React.FC = () => {
-    const [flower, setFlower] = useState("none");
+export const App: React.FC = () => {
+    const [newsState, newsDispatch] = useReducer(newsReducer, {loading: false} as NewsState);
+    const [tickersState, tickersDispatch] = useReducer(tickersReducer, {loading: false} as TickersState);
 
-    useEffect(() => {
-        fetch("/flower")
-            .then(response => response.json())
-            .then(data => {
-                setFlower(data);
-            });
-    }, []);
+    useEffect(
+        () => {
+            scheduleRegularNewsUpdate(newsDispatch);
+            scheduleRegularTickersUpdate(tickersDispatch);
+        },
+        []);
 
     return (
-        <div className="App">
-            <h1>{JSON.stringify(flower)}</h1>
-        </div>
+        <UseNewsReducerContext.Provider value={{state: newsState, dispatch: newsDispatch}}>
+            <UseTickersReducerContext.Provider value={{state: tickersState, dispatch: tickersDispatch}}>
+                <BrowserRouter>
+                    <Switch>
+                        <Route path={"/"} component={NewsCenter}/>
+                        <Redirect to={"/"}/>
+                    </Switch>
+                </BrowserRouter>
+            </UseTickersReducerContext.Provider>
+        </UseNewsReducerContext.Provider>
     );
 };
-
-export default App;
