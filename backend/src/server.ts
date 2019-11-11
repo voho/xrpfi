561e836f2d87fb657631be7bce1fead59528b637
Vojta Hordejcuk <vojtech.hordejcuk@gmail.com>
2019-11-11 22:55:51 +0100

    another fix

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,11 @@
 import express, {Request, Response} from "express";
+import * as path from "path";
 import {getStatus} from "./fetcher/fetcherLoader";
 import {scheduleFetcherRefresh} from "./fetcher/fetcherScheduler";
 import {getNews} from "./store/newsStorage";
 import {logInfo} from "./utils/logger";
 
-const PORT = process.env.HTTP_PORT || 4000;
+const PORT = process.env.HTTP_PORT || 8080;
 const app = express();
 
 scheduleFetcherRefresh();
@@ -16,6 +17,8 @@ app.get("/api/news", (req: Request, res: Response) => {
 app.get("/api/status", (req: Request, res: Response) => {
     res.json({root: getStatus()});
 });
+
+app.use(express.static(path.join(__dirname, "../../frontend/build")));
 
 app.listen(PORT, () => {
     logInfo(`Server listening at port ${PORT}.`);
